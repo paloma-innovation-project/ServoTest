fe154996c74169f47f65ca215fd2a349d6b82712
Shingo Morimoto <shingo.morimoto@paloma.co.jp>
2019-06-14 13:53:55 +0900

    ArduinoServo追加

---
+++ ArduinoServo/ArduinoServo/Class1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArduinoServo
+{
+    public class Servo
+    {
+        System.IO.Ports.SerialPort sp;
+        public Servo(string portname)
+        {
+            sp = new System.IO.Ports.SerialPort();
+            sp.PortName = portname;
+            sp.BaudRate = 9600;
+            sp.Open();
+            Task k = Task.Factory.StartNew(MainLoop);
+        }
+
+        private void MainLoop()
+        {
+            while (true)
+            {
+                byte[] b = { _angle };
+                sp.Write(b, 0, 1);
+                System.Threading.Thread.Sleep(50);
+            }
+        }
+
+        private byte _angle;
+
+        public byte Angle
+        {
+            get
+            {
+                return _angle;
+            }
+            set
+            {
+                _angle = value;
+            }
+        }
+        public void Open()
+        {
+            _angle = 180;
+        }
+
+        public void Close()
+        {
+            _angle = 0;
+        }
+
+    }
+}
